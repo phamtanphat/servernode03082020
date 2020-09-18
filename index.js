878571c68b53875e3a0eec54ebb98c20a43a9f2f
phamtanphat <phatdroid94@gmail.com>
2020-09-18 20:34:33 +0700

    body parse

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+var {json} = require('body-parser')
+const app = express()
+ 
+app.use(json())
+app.get('/cong/:a/:b', function (req, res) {
+//   const name = req.query.name
+//   const name = req.params.name
+    const {a , b} = req.params
+    if(isNaN(a) || isNaN(b)){
+        res.send({success : false ,message : "Giá trị không phải là số"})
+    }else{
+        res.send({success : true , value : parseInt(a) + parseInt(b)})
+    }
+    res.end();
+})
+
+app.post('/tru' , function(req ,res){
+    console.log(req.body)
+})
+ 
+app.listen(3000)
